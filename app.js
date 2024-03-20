0fc391f89179e29a4394d0a8935556ff2039b932
TNTMaster370 <trystynmcote@gmail.com>
2024-03-19 19:51:43 -0600

    Attempted to implement email sending;

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 // framework to allow this to work properly.
 
 const express = require("express");
+// const EMAIL = require("./email.js");
 
 
 const app = express();
@@ -10,4 +11,5 @@ const PORT = 2024;
 
 app.listen(PORT, () => {
     console.log(`connected, port ${PORT}`);
+    // EMAIL.send_email("trystynmcote@gmail.com", "I SENT AN EMAIL");
 });
